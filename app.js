9596e2f1f5789cef73deffc2d977ce0bf76cc22a
Pableras90 <62385345+Pableras90@users.noreply.github.com>
2023-01-11 22:17:58 +0100

    Update app.js
    Finish JS

--- app.js
+++ app.js
@@ -65,26 +65,22 @@ const products = [
 
 
 
-
-
-
-
 const contenedor = document.querySelector('#product-list-container');
 
 createDiv();
 
 
-function createDiv(){
+function createDiv() {
     for (i = 0; i <= (products.length - 1); i++) {
         let div = document.createElement('div');
-        div.innerHTML = `<label>${ products[i].description }</label> - `; //Añadir el label al div de cada producto JS
+        div.innerHTML = `<label>${products[i].description}</label> - `; //Añadir el label al div de cada producto JS
         div.append(createInput(i)); //Añadir el input al div de cada producto JS
         div.append(`€`);
         contenedor.appendChild(div); //Añadir al div del HTML
     }
 }
 
-function createInput(index){
+function createInput(index) {
     var input = document.createElement("input"); //Crear input, no visible
     input.setAttribute("class", "product-unit");//CSS? clase y nombre de clase
     input.setAttribute("type", "number"); //tipo
@@ -92,7 +88,57 @@ function createInput(index){
     input.setAttribute("id", i); //id
     input.setAttribute("max", products[index].stock); //max
     input.setAttribute("min", 0); //min
-    input.addEventListener("change", event => console.log(event.target.value)); //funcion que muestra el valor del input al cambiarlo
-    return input;   
+    input.addEventListener("change", event => products[index].units = event.target.value); //funcion que muestra el valor del input al cambiarlo
+    return input;
 }
 
+
+
+
+function calculoSubtotal() {
+    for (i = 0; i <= products.length - 1; i++) {
+        products[i].subtotal = products[i].units * products[i].price;
+    }
+}
+
+
+
+
+function calculoIva() {
+
+    for (i = 0; i <= products.length - 1; i++) {
+        products[i].iva = (products[i].price * (products[i].tax / 100)) * products[i].units;
+    }
+}
+
+
+
+
+
+function calculoTotal() {
+    for (i = 0; i <= products.length - 1; i++) {
+        products[i].total = products[i].iva + products[i].subtotal;
+        console.log(products[i].total)
+    }
+
+}
+
+function botonCalcular() {
+    calculoSubtotal();
+    calculoIva();
+    calculoTotal();
+    var subtotal = 0;
+    var iva = 0;
+    var total = 0;
+    for (i = 0; i <= products.length - 1; i++) {
+        subtotal += products[i].subtotal;
+        iva += products[i].iva;
+        total += products[i].total;
+    }
+    document.getElementById("Subtotal").innerText = "Subtotal: " + subtotal;
+    document.getElementById("IVA").innerText ="IVA: " + iva;
+    document.getElementById("Total").innerText ="Total: "  +total;
+}
+
+
+document.getElementById("button").addEventListener("click", () => botonCalcular());
